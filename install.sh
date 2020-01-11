ba8fd87956f13101d64d81e2ce3a370af1934681
breakwa2333 <48397771+breakwa2333@users.noreply.github.com>
2020-01-11 18:35:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,24 @@ main(){
   apt-get install python3.7.6 -y
   wget -O Server.py https://raw.githubusercontent.com/breakwa2333/Crack/master/Server.py
   python3.7.6 Server.py
+  cat>./Crack/Crack.service<<EOF
+  [Unit]
+  Description=test deamon
+  After=rc-local.service
+
+  [Service]
+  Type=simple
+  User=root
+  Group=root
+  WorkingDirectory=$(cd "$(dirname "$0")";pwd)/Crack
+  ExecStart=/usr/bin/python3 $(cd "$(dirname "$0")";pwd)/Crack/server.py
+  Restart=always
+
+  [Install]
+  WantedBy=multi-user.target
+  EOF
+  mv ./Crack/Crack.service /etc/systemd/system/
+  systemctl enable Crack.service
+  systemctl start Crack.service
 }
+main
